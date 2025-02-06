2ef50a30be6ef3ddccd0d304412ae6598d8ad14b
Incivin <slava.boldyrev@inbox.ru>
2025-02-06 19:05:33 +0500

    добавлены изменения для POST и GET

--- main.go
+++ main.go
@@ -3,27 +3,42 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 var task string
 
-type requestBody struct {
-	Message string `json:"message"`
+func CreateMessage(w http.ResponseWriter, r *http.Request) {
+	var msg Message
+	err := json.NewDecoder(r.Body).Decode(&msg)
+	if err != nil {
+		fmt.Fprintln(w, "Json error")
+		return
+	}
+	if err := DB.Create(&msg).Error; err != nil {
+		fmt.Fprintln(w, "Fail add DB")
+		return
+	}
+	fmt.Fprintln(w, "Записано в message", msg.Task)
 }
-
-func PostHandler(w http.ResponseWriter, r *http.Request) {
-	rBody := requestBody{}
-	json.NewDecoder(r.Body).Decode(&rBody)
-	task = rBody.Message
-}
-func GetHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello", task)
+func GetMessages(w http.ResponseWriter, r *http.Request) {
+	var message []Message
+	if err := DB.Find(&message).Error; err != nil {
+		fmt.Fprintln(w, "Fail find DB")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(message)
 }
 
 func main() {
-	http.HandleFunc("/get", GetHandler)
-	http.HandleFunc("/post", PostHandler)
-	http.ListenAndServe("localhost:8080", nil)
+	InitDB()
+	DB.AutoMigrate(&Message{})
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
+	router.HandleFunc("/api/messages", GetMessages).Methods("GET")
+	http.ListenAndServe(":8080", router)
 
 }

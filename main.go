dbafd1079c854a226bcd90260811db6978415125
Incivin <slava.boldyrev@inbox.ru>
2025-02-10 20:17:46 +0500

    implementing a full-fledged CRUD

--- main.go
+++ main.go
@@ -4,10 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
-
-var task string
 
 func CreateMessage(w http.ResponseWriter, r *http.Request) {
 	var msg Message
@@ -20,25 +19,72 @@ func CreateMessage(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Fail add DB")
 		return
 	}
-	fmt.Fprintln(w, "Записано в message", msg.Task)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(msg)
+
 }
 func GetMessages(w http.ResponseWriter, r *http.Request) {
-	var message []Message
-	if err := DB.Find(&message).Error; err != nil {
+	var msg []Message
+	if err := DB.Find(&msg).Error; err != nil {
 		fmt.Fprintln(w, "Fail find DB")
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(message)
+	json.NewEncoder(w).Encode(msg)
+}
+
+func UpdateMessage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var msg Message
+	if err := DB.First(&msg, id).Error; err != nil {
+		fmt.Fprintln(w, "Message not found")
+		return
+	}
+	var updateData Message
+	if err := json.NewDecoder(r.Body).Decode(&updateData); err != nil {
+		fmt.Fprintln(w, "Invalid JSON")
+		return
+	}
+	if updateData.Task != "" {
+		msg.Task = updateData.Task
+	}
+	msg.IsDone = updateData.IsDone
+
+	if err := DB.Save(&msg).Error; err != nil {
+		fmt.Fprintln(w, "Fail to update task")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(msg)
+
+}
+func DeleteMessage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var msg Message
+	if err := DB.First(&msg, id).Error; err != nil {
+		http.Error(w, "Message not found", http.StatusNotFound)
+		return
+	}
+	if err := DB.Delete(&msg).Error; err != nil {
+		http.Error(w, "Fail to delete task", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+
 }
 
 func main() {
 	InitDB()
 	DB.AutoMigrate(&Message{})
-
 	router := mux.NewRouter()
+	router.HandleFunc("/api/messages/{id}", UpdateMessage).Methods("PATCH")
+	router.HandleFunc("/api/messages/{id}", DeleteMessage).Methods("DELETE")
 	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
 	router.HandleFunc("/api/messages", GetMessages).Methods("GET")
-	http.ListenAndServe(":8080", router)
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal("Error starting server:", err)
+	}
 
 }
